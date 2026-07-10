4aa3623fed57f66f07fb7953278d0a57a7910ce3
Jung TaeSeung <mike415415@naver.com>
2026-07-11 00:29:59 +0900

    init : project setting

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  
+  return(
+    <>
+    <header>
+      포트폴리오 사이트입니다.
+    </header>
+    <nav>
+      네비게이션
+    </nav>
+
+    <section>
+      섹션입니다.
+    </section>
+  </>
+  )
+}
+
+export default App
